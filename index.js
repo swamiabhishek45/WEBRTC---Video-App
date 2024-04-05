3b4e8ade100dbf7c5064fd821afdff8994f692d1
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-05 21:32:59 +0530

    i cra

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const app =  require("express")();
+const app = require("express")();
 const server = require("http").createServer(app);
-const cors = require('cors'); // userful to enable CORS
+const cors = require("cors"); // userful to enable CORS
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
-    }
-})
+        methods: ["GET", "POST"],
+    },
+});
 
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Chal gaya hu mein");
+});
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.io("calluser", ({ userToCall, signalData, from, name }) => {
+        io.on(
+            userToCall().emit("calluser", { signal: signalData, from, name })
+        );
+    });
+
+    socket.io("answercall", (data) => {
+        io.on(data.to).emit("callaccepted", data.signal);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
+
