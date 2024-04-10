5d5b2748864b621746d6db05a84d43f9a5c7fe0d
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-10 13:38:03 +0530

    socket.io part completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import {Routes, Route} from 'react-router-dom';
-import './App.css';
-import Lobby from './screens/Lobby';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Lobby from "./screens/Lobby";
+import Room from "./screens/Room";
 
 function App() {
-  return (
-    <div className="App">
-    <Routes>
-      <Route path='/' element={<Lobby /> }></Route>
-    </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Lobby />}></Route>
+                <Route path="/room/:roomId" element={<Room />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,16 +1,36 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useSocket } from "../context/SocketProvider";
+import {useNavigate} from 'react-router-dom';
 
 const Lobby = () => {
     const [email, setEmail] = useState("");
     const [room, setRoom] = useState("");
 
+    const socket = useSocket();
+    const navigate = useNavigate();
+
     const handleSubmitForm = useCallback(
         (e) => {
             e.preventDefault();
-            console.log({ email, room });
+            socket.emit("room:join", {email, room})
         },
-        [email, room]
+        [email, room, socket]
     );
+
+    const handleJoinRoom = useCallback((data) => {
+        const {email, room} = data;
+        navigate(`/room/${room}`)
+
+    },[navigate]);
+
+    useEffect(() => {
+        socket.on("room:join", handleJoinRoom);
+
+        return () => {
+            socket.off("room:join", handleJoinRoom);
+        };
+    }, [socket, handleJoinRoom]);
+
     return (
         <div>
             <h1>Lobby</h1>
