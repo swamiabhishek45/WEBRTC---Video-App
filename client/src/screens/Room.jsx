0e2e2916315c44820435c513a271d4b55d5cf57f
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-10 14:23:26 +0530

    Add react-player for video call feature

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,14 +1,25 @@
 import React, { useCallback, useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 import { useSocket } from "../context/SocketProvider";
 
 const Room = () => {
     const socket = useSocket();
     const [remoteSocketId, setRemoteSocketId] = useState(null);
+    const [myStream, setMyStream] = useState();
 
     const handleUserJoined = useCallback(({ email, id }) => {
         console.log(email, id);
         setRemoteSocketId(id);
     });
+
+    const handleCallUser = useCallback(async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true,
+        });
+
+        setMyStream(stream);
+    }, []);
 
     useEffect(() => {
         socket.on("user:joined", handleUserJoined);
@@ -21,7 +32,20 @@ const Room = () => {
     return (
         <>
             <h1>Room</h1>
-            <h4>{remoteSocketId ? "Connected": "No one in room"}</h4>
+            <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
+            {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
+            {myStream && (
+                <>
+                    <h1>My Stream</h1>
+                    <ReactPlayer
+                        playing
+                        muted
+                        height="300px"
+                        width="300px"
+                        url={myStream}
+                    />
+                </>
+            )}
         </>
     );
 };
